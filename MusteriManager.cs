0302644626ee7bc85342135bfd5ee4db1d6147bf
Esra Erkan <esraerkan38@outlook.com>
2021-01-26 22:01:56 +0300

    3.hafta ödev

---
+++ MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Adi + " " +musteri.Soyadi +" sisteme eklenmiştir.");
+        }
+        public void Sil(Musteri musteri)
+        {
+
+            Console.WriteLine(musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi +" sisitemden silinmiştir.");
+        }
+        public void Listele(Musteri musteri)
+        {
+            
+            Console.WriteLine("Müşteri Numarası : " + musteri.Id);
+            Console.WriteLine("Müşteri Adı Soyadı : " + musteri.Adi + " " +musteri.Soyadi);
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 3381;
+            musteri1.Adi = "Esra";
+            musteri1.Soyadi = "ERKAN";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 3381;
+            musteri2.Adi = "Ebru";
+            musteri2.Soyadi = "AKIN";
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3381;
+            musteri3.Adi = "Zemzem";
+            musteri3.Soyadi = "ULUTÜRK";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine("***Ekleme***");
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            Console.WriteLine("***SİLME***");
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Sil(musteri3);
+
+            Console.WriteLine("***LİSTELEME***");
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+
+
+
+
+
+        }
+    }
+}
